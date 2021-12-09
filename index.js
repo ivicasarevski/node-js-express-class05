adeada8f1dde89acab6be83aa82f5c6ecd136aeb
ivicasarevski <ivica.sarevski@yahoo.com>
2021-12-09 20:24:55 +0100

    third one

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello!");
+//const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next) => {
+    console.log('This is first one');
+})
+
+app.use((req,res,next) => {
+    console.log('This is second one')
+})
+
+
+
+app.listen(3000);
