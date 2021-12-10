30b19ef660648b20055c3ebd0ce18f2de9f9f1f1
ivicasarevski <ivica.sarevski@yahoo.com>
2021-12-10 14:44:44 +0100

    new commit

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 //const http = require('http');
 
+const bodyParser = require('body-parser');
+
 const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('This is first one');
-})
+app.use(express.urlencoded({ extended: false }))
 
 app.use((req,res,next) => {
-    console.log('This is second one')
+    console.log('This is a middleware');
+    next();
+})
+
+app.use('/add-animal', (req,res,next) => {
+    
+    res.send(`<form action="/animal" method="POST">
+                <input type="text" name="animalName" />
+                <button type="submit">Submit</button>
+              </form>`)
+              
+})
+
+app.post('/animal', (req,res,next) => {
+    console.log('BODY:', req.body)
+    res.redirect('/')
+})
+
+
+app.use('/', (req,res,next) => {
+    res.send(`<h1>Hello World!</h1>`)
 })
 
 
